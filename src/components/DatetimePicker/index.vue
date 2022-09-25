164e9288cfe9685b003a3cf07f1186a300a1fad1
liu <liubo@jeeshare.com>
2022-09-25 17:04:02 +0800

    添加配置项

---
+++ src/components/DatetimePicker/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>date</div>
+</template>
+
+<script setup>
+import { defineProps,defineEmits } from "vue"
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'date',
+  },
+  modelValue: Date,
+  minDate: {
+    type: Date,
+    default: () => new Date(new Date().getFullYear() - 10, 0, 1),
+    validator: (val) => {
+      return Object.prototype.toString.call(val) === '[object Date]' && !Number.isNaN((val).getTime());
+    }
+  },
+  maxDate: {
+    type: Date,
+    default: () => new Date(new Date().getFullYear() + 10, 11, 31),
+    validator: (val) => {
+      return Object.prototype.toString.call(val) === '[object Date]' && !Number.isNaN((val).getTime());
+    }
+  },
+})
+const emit = defineEmits('')
+</script>
+
+<style lang="less" scoped>
+
+</style>
